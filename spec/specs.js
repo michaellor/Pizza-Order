536a3a5a299e2d86ef9e7b0f658dbbecd54f57e9
Michael Lor <michael.vang.1985@gmail.com>
2016-01-29 08:52:17 -0800

    add functionality to determine price based on pizza size.

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,15 @@
 describe('Pizza', function() {
   it("creates a pizza object to store properties.", function() {
-  var testPizza = new Pizza ("Large","Pepperoni");
+  var testPizza = new Pizza ("Large",2);
   expect(testPizza.pizzaSize).to.equal("Large");
-  expect(testPizza.pizzaTopping).to.equal("Pepperoni");
+  expect(testPizza.pizzaTopping).to.equal(2);
+  });
+  it("will adjust the price based on pizza size.", function() {
+    var testPizza = new Pizza ("Large",2);
+    expect(testPizza.pizzaSizePrice()).to.equal(12.00);
+  });
+  it("will determine the additional price of toppings.", function() {
+    var testPizza = new Pizza ("Large",2);
+    expect(testPizza.pizzaToppingPrice()).to.equal(1.00);
   });
 });
