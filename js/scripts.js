ebb541eb78c7b56a58c266864d59f6c630dfceb5
Michael Lor <michael.vang.1985@gmail.com>
2016-01-29 09:10:43 -0800

    refactor 3 price functions into a single function

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,6 @@ function Pizza (pizzaSize, pizzaTopping) {
   this.pizzaPriceTotal = [];
 }
 
-Pizza.prototype.pizzaSizePrice = function() {
-
-  var price = 0;
-  if (this.pizzaSize === "Large") {
-    price = 12.00;
-  } else if (this.pizzaSize === "Medium") {
-    price = 10.00;
-  } else if (this.pizzaSize === "Small") {
-    price = 8.00;
-  }
-  console.log(price);
-  return price;
-}
-
-Pizza.prototype.pizzaToppingPrice = function() {
-
-  var priceTopping = 0;
-  priceTopping = (this.pizzaTopping * .50)
-  return priceTopping;
-}
-
 Pizza.prototype.pizzaTotalPrice = function() {
 
   var totalPrice = 0;
--- spec/specs.js
+++ spec/specs.js
@@ -4,14 +4,6 @@ describe('Pizza', function() {
   expect(testPizza.pizzaSize).to.equal("Large");
   expect(testPizza.pizzaTopping).to.equal(2);
   });
-  it("will adjust the price based on pizza size.", function() {
-    var testPizza = new Pizza ("Large",2);
-    expect(testPizza.pizzaSizePrice()).to.equal(12.00);
-  });
-  it("will determine the additional price of toppings.", function() {
-    var testPizza = new Pizza ("Large",2);
-    expect(testPizza.pizzaToppingPrice()).to.equal(1.00);
-  });
   it("will find the total pizza price by adding size price to the toppings price.", function() {
     var testPizza = new Pizza ("Large",2);
     expect(testPizza.pizzaTotalPrice()).to.equal(13.00);
