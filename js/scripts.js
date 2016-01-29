3bcb39defa3bd774bd2075b90f5c021916635ad0
Michael Lor <michael.vang.1985@gmail.com>
2016-01-29 09:06:06 -0800

    add functionality for total price by refactoring Size price and Toppings price functions

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,24 @@ Pizza.prototype.pizzaToppingPrice = function() {
   priceTopping = (this.pizzaTopping * .50)
   return priceTopping;
 }
+
+Pizza.prototype.pizzaTotalPrice = function() {
+
+  var totalPrice = 0;
+  var price = 0;
+  var priceTopping = 0;
+
+  if (this.pizzaSize === "Large") {
+    price = 12.00;
+  } else if (this.pizzaSize === "Medium") {
+    price = 10.00;
+  } else if (this.pizzaSize === "Small") {
+    price = 8.00;
+  }
+
+  priceTopping = (this.pizzaTopping * .50)
+
+  totalPrice = price + priceTopping;
+
+  return totalPrice;
+}
